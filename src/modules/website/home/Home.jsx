9dd450a82c4d3e72b23031dcd3a87c9002f0e4d2
mianahmad93 <mianahmad93000@gmail.com>
2025-05-22 13:22:49 +0500

    Ahmed: Home page h1 added

--- src/modules/website/home/Home.jsx
+++ src/modules/website/home/Home.jsx
@@ -4,7 +4,11 @@ const Home = () => {
     return (
         <>
             <Navbar />
-            
+            <div className='full-width banner-hero bg-orange-100 banner-1'>
+                <div className='px-[12px] md:px-[36px] xl:px-0 mt-0 z-10 relative mx-auto py-[100px] max-w-[1320px] lg:flex lg:items-center'>
+                    <h1>hello world!</h1>
+                </div>
+            </div>
 
         </>
     )
