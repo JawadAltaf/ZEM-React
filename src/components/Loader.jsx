8d3deb1ab2aaf950832c327f72d3cf2d01fce5f2
mianahmad93 <mianahmad93000@gmail.com>
2025-05-23 17:31:36 +0500

    ahmad:add loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import '../styles/index.css';
+
+const Loader = () => {
+    return (
+        <>
+            <div className="loader"></div>
+        </>
+    )
+}
+
+export default Loader
--- src/modules/website/home/Home.jsx
+++ src/modules/website/home/Home.jsx
@@ -1,25 +1,44 @@
+import { useEffect, useState } from 'react';
 import Card from '../../../components/Card';
 import Goto_Top from '../../../components/Goto_Top';
 import HomeSlider from '../../../components/HomeSlider';
+import Loader from '../../../components/Loader';
 import Navbar from '../../../components/Navbar';
 import TestimonialSlider from '../../../components/Slider';
 
 import WhoWeSec from '../../../components/WhoWeSec';
 import WhyChoose from '../../../components/WhyChoose';
 
+
 const Home = () => {
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+    }, [])
+
     return (
         <>
 
-            <Navbar />
-            <HomeSlider />
-            <Card />
-            <WhoWeSec />
-            <Goto_Top />
-            <WhyChoose />
-            <TestimonialSlider/>
-            
-            
+            {
+                loading ? <Loader /> :
+                    <>
+                        <Navbar />
+                        <HomeSlider />
+                        <Card />
+                        <WhoWeSec />
+                        <Goto_Top />
+                        <WhyChoose />
+                        <TestimonialSlider />
+                    </>
+            }
+
+
+
+
+
 
 
         </>
